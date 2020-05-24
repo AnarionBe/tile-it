4343a10abe2ad6ab9971e79e794a775dbfb82bea
Anarion <marcodb.debona@gmail.com>
2020-05-24 10:41:17 +0200

    grid system

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import {Home} from './containers/index'
 
+import config from '../configs/app.json'
+
 export default () => {
   return (
-    <Home />
+    <div className="app">
+      <Home layout={config.layout} />
+    </div>
   );
 }
--- src/js/containers/home.jsx
+++ src/js/containers/home.jsx
@@ -4,10 +4,24 @@ import React from 'react'
  * Displaying module in a grid according to the config
  */
 
-export const Home = ({}) => {
+export const Home = ({layout}) => {
+  const generateLayout = () => {
+    return layout.map((e, i) => {
+      const style = {
+        gridColumnStart: e.origin[0],
+        gridColumnEnd: `span ${e.size[0]}`,
+        gridRowStart: e.origin[1],
+        gridRowEnd: `span ${e.size[1]}`,
+        border: '1px black solid'
+      };
+
+      return (<div style={style} key={i}>{e.name + i}</div>);
+    });
+  }
+
   return (
     <div className="home">
-      Home Container
+      {generateLayout()}
     </div>
   );
 }
